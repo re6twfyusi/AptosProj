555c8104847d91a571f921bcdde0891a3492d8c1
re6twfyusi <dsjcaevonbsmith@hotmail.com>
2024-05-14 05:31:09 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function maxSubArray(nums) {
-  let maxSum = nums[0];
-  let currentSum = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    currentSum = Math.max(nums[i], currentSum + nums[i]);
-    maxSum = Math.max(maxSum, currentSum);
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
   }
-  return maxSum;
+  return minHeap.size();
 }
