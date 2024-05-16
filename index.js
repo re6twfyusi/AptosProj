a170a80415a5a837d1ada184b2612ebc6f5fd110
re6twfyusi <dsjcaevonbsmith@hotmail.com>
2024-05-16 14:58:56 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = triangle[n - 1];
-  for (let i = n - 2; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
+const fibonacciRecursive = (n) => {
+  if (n <= 1) {
+    return n;
   }
-  return dp[0];
-}
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
